90a57ddf2ffdbf4e4b759063ae509ebab0c2e3a0
dgebert <dominik.gebert@symmetry.pl>
2018-06-14 14:14:19 +0200

    DEFINING THE COMPONENT METADATA PROPERTIES

--- modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
+++ modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
@@ -21,18 +21,20 @@ import java.util.logging.Logger;
  * @author DominikGebert
  */
 @Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=category.sample",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=guestbook-web Portlet",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.name=" + GuestbookPortletKeys.Guestbook,
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
+		immediate = true,
+		property = {
+				"com.liferay.portlet.display-category=category.social",
+				"com.liferay.portlet.instanceable=false",
+				"com.liferay.portlet.scopeable=true",
+				"javax.portlet.display-name=Guestbook",
+				"javax.portlet.expiration-cache=0",
+				"javax.portlet.init-param.template-path=/",
+				"javax.portlet.init-param.view-template=/guestbookwebportlet/view.jsp",
+				"javax.portlet.resource-bundle=content.Language",
+				"javax.portlet.security-role-ref=power-user,user",
+				"javax.portlet.supports.mime-type=text/html"
+		},
+		service = Portlet.class
 )
 public class GuestbookPortlet extends MVCPortlet {
 
