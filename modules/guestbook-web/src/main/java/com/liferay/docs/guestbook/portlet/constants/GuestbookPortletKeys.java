818176029b8cb8209ebac6ede5e221de451f5912
dgebert <dominik.gebert@symmetry.pl>
2018-06-14 12:31:06 +0200

    WRITING YOUR FIRST LIFERAY PORTAL APPLICATION

---
+++ modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/constants/GuestbookPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.docs.guestbook.portlet.constants;
+
+/**
+ * @author DominikGebert
+ */
+public class GuestbookPortletKeys {
+
+	public static final String Guestbook = "Guestbook";
+
+}
---
+++ modules/guestbook-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="guestbook-web.caption"/></b>
+</p>
