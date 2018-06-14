262fee726a9851325a389366b859de3ce7d14414
dgebert <dominik.gebert@symmetry.pl>
2018-06-14 12:36:09 +0200

    IMPLEMENTING PORTLET ACTIONS

--- modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
+++ modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
@@ -4,9 +4,16 @@ import com.liferay.docs.guestbook.portlet.constants.GuestbookPortletKeys;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
-import javax.portlet.Portlet;
+import javax.portlet.*;
 
+import com.liferay.portal.kernel.util.ParamUtil;
 import org.osgi.service.component.annotations.Component;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author DominikGebert
@@ -26,4 +33,47 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class GuestbookPortlet extends MVCPortlet {
+
+	public void addEntry(ActionRequest request, ActionResponse response) {
+		try {
+			PortletPreferences prefs = request.getPreferences();
+
+			String[] guestbookEntries = prefs.getValues("guestbook-entries",
+					new String[1]);
+
+			ArrayList<String> entries = new ArrayList<String>();
+
+			if (guestbookEntries[0] != null) {
+				entries = new ArrayList<String>(Arrays.asList(prefs.getValues(
+						"guestbook-entries", new String[1])));
+			}
+
+			String userName = ParamUtil.getString(request, "name");
+			String message = ParamUtil.getString(request, "message");
+			String entry = userName + "^" + message;
+
+			entries.add(entry);
+
+			String[] array = entries.toArray(new String[entries.size()]);
+
+			prefs.setValues("guestbook-entries", array);
+
+			try {
+				prefs.store();
+			}
+			catch (IOException ex) {
+				Logger.getLogger(GuestbookPortlet.class.getName()).log(
+						Level.SEVERE, null, ex);
+			}
+			catch (ValidatorException ex) {
+				Logger.getLogger(GuestbookPortlet.class.getName()).log(
+						Level.SEVERE, null, ex);
+			}
+
+		}
+		catch (ReadOnlyException ex) {
+			Logger.getLogger(GuestbookPortlet.class.getName()).log(
+					Level.SEVERE, null, ex);
+		}
+	}
 }
