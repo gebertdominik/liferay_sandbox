95c7699ab3eb692cb980ad59942145ac267080d0
dgebert <dominik.gebert@symmetry.pl>
2018-06-14 14:29:49 +0200

    CREATING PORTLET KEYS

--- modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
+++ modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
@@ -31,7 +31,8 @@ import java.util.logging.Logger;
 				"javax.portlet.init-param.template-path=/",
 				"javax.portlet.init-param.view-template=/guestbookwebportlet/view.jsp",
 				"javax.portlet.resource-bundle=content.Language",
-				"javax.portlet.security-role-ref=power-user,user",
+                "javax.portlet.name=" + GuestbookPortletKeys.GUESTBOOK,
+                "javax.portlet.security-role-ref=power-user,user",
 				"javax.portlet.supports.mime-type=text/html"
 		},
 		service = Portlet.class
